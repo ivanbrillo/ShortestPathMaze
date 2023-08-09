16b4ff7871b06f81b4c670f684fbdeb63c65a185
ivanbrillo <ivan.brillo@raceup.it>
2023-08-09 14:03:10 +0200

    finish the refactor

--- src/BoardUI.java
+++ src/BoardUI.java
@@ -6,18 +6,37 @@ public class BoardUI extends JPanel {
     static int dimension = 30;
     private final Maze maze;
 
-    public BoardUI(Maze maze){
+    public BoardUI(Maze maze) {
         this.maze = maze;
         setSize(dimension * (maze.board[0].length) + 16, dimension * (maze.board.length) + 38);
     }
 
     public void paintComponent(Graphics g) {
 
+        // print background
         g.setColor(Color.BLUE);
         for (int i = 0; i < maze.board.length; i++)
             for (int j = 0; j < maze.board[0].length; j++)
                 if (maze.board[i][j])
                     g.fillRect(j * dimension, i * dimension, dimension, dimension);
+
+        // print visited
+        g.setColor(Color.GREEN);
+        for (MazeBlock p : maze.visited)
+            g.fillRect(p.currentPoint.x * dimension, p.currentPoint.y * dimension, dimension, dimension);
+
+        // print the best path
+        g.setColor(Color.BLACK);
+        MazeBlock last = maze.visited.get(maze.visited.size() - 1);
+        while (last.parent != null) {
+            last = last.parent;
+            g.fillRect(last.currentPoint.x * dimension + dimension / 4, last.currentPoint.y * dimension + dimension / 4, dimension / 2, dimension / 2);
+        }
+
+        // print start and end
+        g.setColor(Color.RED);
+        g.fillRect(maze.start.x * dimension, maze.start.y * dimension, dimension, dimension);
+        g.fillRect(maze.end.x * dimension, maze.end.y * dimension, dimension, dimension);
 
     }
 
--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Maze {
@@ -26,12 +27,22 @@ public class Maze {
             {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true},
             {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}
     };
+    private final ArrayList<Point> teleport = new ArrayList<>(Arrays.asList(new Point(0, 9), new Point(18, 9)));
+    private final int[][] translations = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0},
+    };
+    public Point end;
+    public Point start;
+    public ArrayList<MazeBlock> visited = new ArrayList<>();
 
-    Point end;
-    Point start;
-    Point[] teleport = {new Point(9, 0), new Point(9, 18)};
 
     public Maze(String[] args) {
+
+        if (args.length != 4)
+            throw new RuntimeException();
 
         try {
             start = new Point(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
@@ -39,24 +50,94 @@ public class Maze {
 
             Rectangle rect = new Rectangle(board[0].length, board.length);
 
-            if (!(rect.contains(start) && rect.contains(end)))
+            if (!(rect.contains(start) && rect.contains(end)) || isOccupied(board, start) || isOccupied(board, end))
                 throw new RuntimeException();
 
         } catch (Exception e) {
             throw new RuntimeException();
         }
 
+        solveMaze();
+
     }
 
-    public boolean[][] getCopy() {
+    private boolean[][] getCopy() {
         return Arrays.stream(board).map(boolean[]::clone).toArray(boolean[][]::new);
     }
 
-    public void percorso() {
+    private boolean isOccupied(boolean[][] maze, Point point) {
 
-        boolean[][] maze_copy = getCopy();
+        if (point.y < 0 || point.x < 0 || point.y >= maze.length || point.x >= maze[0].length)
+            return true;
 
-
+        return maze[point.y][point.x];
     }
 
+    private boolean visitNeighbour(boolean[][] maze, ArrayList<MazeBlock> frontier) {
+
+        for (int[] translation : translations) {
+
+            Point p = new Point(frontier.get(0).currentPoint);
+            p.translate(translation[0], translation[1]);
+
+            if (!isOccupied(maze, p)) {
+                MazeBlock mazeBlock = new MazeBlock(p, frontier.get(0));
+                frontier.add(mazeBlock);
+                visited.add(mazeBlock);
+                maze[p.y][p.x] = true;
+
+                if (p.equals(end))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkTeleport(boolean[][] maze, ArrayList<MazeBlock> frontier) {
+
+        MazeBlock block = frontier.get(0);
+
+        if (teleport.contains(block.currentPoint)) {
+            MazeBlock teleportedBlock = new MazeBlock(teleport.get((teleport.indexOf(block.currentPoint) + 1) % 2), block);
+            visited.add(teleportedBlock);
+            maze[teleportedBlock.currentPoint.y][teleportedBlock.currentPoint.x] = true;
+            frontier.add(teleportedBlock);
+        }
+
+        return frontier.get(frontier.size() - 1).currentPoint.equals(end);
+    }
+
+    public void solveMaze() {
+
+        if (start.equals(end))
+            return;
+
+        boolean[][] maze_copy = getCopy();
+        ArrayList<MazeBlock> frontier = new ArrayList<>();
+        frontier.add(new MazeBlock(start, null));
+
+        while (frontier.size() > 0) {
+
+            if (visitNeighbour(maze_copy, frontier))
+                return;
+
+            if (checkTeleport(maze_copy, frontier))
+                return;
+
+            frontier.remove(0);
+
+        }
+    }
+}
+
+class MazeBlock {
+
+    Point currentPoint;
+    MazeBlock parent;
+
+    public MazeBlock(Point current, MazeBlock parent) {
+        currentPoint = current;
+        this.parent = parent;
+
+    }
 }
